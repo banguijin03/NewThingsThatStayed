2dc631a0b0effb5b558750b76160e5c640c15d9b
dreamzoe <dreamzoe04@gmail.com>
2026-05-19 17:01:18 +0900

    ui

---
+++ Assets/0.Scripts/Objects/Controllers/PlayerController.cs
@@ -0,0 +1,31 @@
+using System;
+using UnityEngine;
+
+public class PlayerController : ControllerBase
+{
+    protected override void OnPossess(CharacterBase newCharacter)
+    {
+        base.OnPossess(newCharacter);
+        InputManager.OnMouseRightButton -= MoveToMousePosition;
+        InputManager.OnMouseRightButton += MoveToMousePosition;
+        InputManager.OnMove -= MoveToDirection;
+        InputManager.OnMove += MoveToDirection;
+    }
+
+    protected override void OnUnpossess(CharacterBase oldCharacter)
+    {
+        base.OnUnpossess(oldCharacter);
+        InputManager.OnMouseRightButton -= MoveToMousePosition;
+        InputManager.OnMove -= MoveToDirection;
+    }
+
+    public void MoveToMousePosition(bool value, Vector2 screenPosition, Vector3 worldPosition)
+    {
+        if (value) CommandMoveToDestination(worldPosition, 0.0f);
+    }
+
+    public void MoveToDirection(Vector2 value)
+    {
+        CommandMoveToDirection(value);
+    }
+}
